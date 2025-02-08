2d31b0172f5ac3cad9205da141be26d563f1f9e8
Juliano Hiroi <julianohiroi@gmail.com>
2025-02-08 10:21:15 -0300

    Adicionei envio de sinal do cliente

--- client/client.py
+++ client/client.py
@@ -1,29 +1,79 @@
 import requests
 import time
+import sys
+import os
+import torch
+import pandas as pd
+import json
+import numpy as np
+import random
+
 
 # URL do servidor Flask
-url = "http://127.0.0.1:5000/processar"
+URL = "http://127.0.0.1:5000/processar"
+
+TIPO_ALGORITMO = ["cgne", "cgnr"]
+
+TIPO_SINAIS = ["G-1"]
+
+sinais = {
+    "G-1": { "name": "G-1", "path": "./client/signal/G-1.csv", "shape": (50816, 1) , "S": 794 , "N": 64}
+}
+
+# Configurar o dispositivo (CPU ou GPU)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Dispositivo: {device}")
+
+
+def load_csv_to_tensor(file_path, expected_shape=None, sep=";", device=""):
+    data = pd.read_csv(file_path, header=None, sep=sep)
+    data = data.apply(pd.to_numeric, errors='coerce').fillna(0)
+    tensor = torch.tensor(data.values, dtype=torch.float32, device=device)
+
+    if expected_shape and tensor.shape != expected_shape:
+        raise ValueError(f"Arquivo {file_path} tem forma {tensor.shape}, mas esperava {expected_shape}")
+
+    return tensor
 
 # Função que simula uma sequência de sinais
-def enviar_sinais():
-    sinais = [
-        "Sinal 1",
-        "Sinal 2",
-        "Sinal 3"
-    ]
+def enviar_sinal(sinal , algoritmo):
+    array_sinal = sinal["tensor"].tolist()
+    data = {
+        "sinal": array_sinal,
+        "algoritmo": algoritmo,
+        "shape": sinal["shape"]
+    }
+    json_data = json.dumps(data)
+    response = requests.post(URL, json=json_data)
+    return response.json()
+
+def ganho_sinal(sinal):
+    g = sinal["tensor"]
+    for c in range( sinal["N"]):
+        for i in range(sinal["S"]):
+            Yi = 100 + 1/20 * i * np.sqrt(i)
+            g[i + c * sinal["S"]] = g[i + c * sinal["S"]] * Yi
+    return g
+
+def carrega_sinais():
+    for key in sinais:
+        sinal = sinais[key]
+        sinal["tensor"] = load_csv_to_tensor(sinal["path"], sinal["shape"], device=device)
+        sinal["tensor"] = ganho_sinal(sinal)
+        
+
+
+def main():
+    carrega_sinais()
     
-    for sinal in sinais:
-        # Envia o sinal para o servidor
-        response = requests.post(url, data=sinal)
-        
-        # Verifica a resposta do servidor
-        if response.status_code == 202:
-            print(f"Sinal enviado: {sinal}")
-            print("Resposta do servidor:", response.json())
-        else:
-            print(f"Falha ao enviar sinal: {sinal}")
-        
-        time.sleep(3)
+    for i in range(1):
+        tipo_sinal = random.choice(TIPO_SINAIS)
+        tipo_algoritmo = random.choice(TIPO_ALGORITMO)
+        response = enviar_sinal(sinais[tipo_sinal], tipo_algoritmo)
+        print(response)
+    
+    
+    
 
 if __name__ == "__main__":
-    enviar_sinais()
+    main()
--- server/server.py
+++ server/server.py
@@ -1,23 +1,28 @@
 from flask import Flask, request, jsonify
 import threading
+import torch
+import json
 
 app = Flask(__name__)
 
 def process_data(data):
-    print(f"Dados recebidos: {data}")
+    print(data)
     response = "Sinal recebido com sucesso!"
     return response
+    
 
 @app.route('/processar', methods=['POST'])
 def handle_client():
-    data = request.get_data(as_text=True)
-    print(f"Dados recebidos: {data}")
-    
+    data_str = request.get_json()  # Garante que os dados sejam interpretados como JSON
+    if data_str is None:
+        return jsonify({"error": "Dados inválidos ou ausentes"}), 400
+    data = json.loads(data_str)
+
     # Cria uma thread para processar os dados de forma assíncrona
     thread = threading.Thread(target=process_data, args=(data,))
     thread.start()
     
-    # Retorna uma resposta imediatamente
+    
     return jsonify({"message": "Dados recebidos e em processamento!"}), 202
 
 if __name__ == "__main__":
